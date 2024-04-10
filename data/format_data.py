bbc7f70b2aafd12a6ef67cb07abaa00d174f04dc
Vedant Vohra <vohra.vedant@gmail.com>
2024-04-09 22:29:22 -0400

    DoneWithM0

--- data/format_data.py
+++ data/format_data.py
@@ -6,9 +6,10 @@ lines = f1.readlines()
 
 # Create the dictionary here
 dictionary ={}
+dict_yearly_avg = {} # Add a dictionary specifically for yearly average, maybe also add one for monthyl
 
 for date in lines:
-    data_shelter = date.split(',') #each arrest with details as elements in a list
+    data_shelter = date.split(',')
 
     dict_input = data_shelter[1:]
     dictionary[data_shelter[0]] = dict_input
